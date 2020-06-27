591adb2f485cbc34735d31cd59468c3ae0c40b5a
wiktor.kobiela <wuch.mail@gmail.com>
2020-06-27 14:52:07 +0200

    ref #controller started CLI

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -1,7 +1,9 @@
 package controller;
 
+import exception.MyException;
 import model.User;
 
+import java.util.Scanner;
 import java.util.function.Consumer;
 
 // UserControler - klasa kontrolera -> klasa obsługująca i dostarczająca logikę biznesową aplikacji
@@ -31,15 +33,23 @@ public class UserController  implements UserControllerTemplate{
     public void updatePasswordByUserId(int userId, String newPassword) {
         try{        // zawiera linijki kodu gdzie może wystąpić wyjątek
         User user = findUserById(userId);
-        user.setPassword(newPassword);
-        } catch (NullPointerException e){      // argumentem w blocku catch jest klasa wyjątku
-            System.out.println("Brak użytkownika o ID " + userId);
-            System.out.println();
-            // e.printStackTrace();         // metoda drukująca na konsoli systemowy komunikat błędu
+        if (user == null){
+            throw new MyException();            // rzucanie wyjątku -> utworzenie obiektu klasy wyjątku
         }
-        catch(Exception e){
-            System.out.println("Wystąpił inny błąd");
+            user.setPassword(newPassword);
         }
+        catch(MyException e){
+            System.out.println("Obsługa mojego wyjątku.");
+            e.printStackTrace();
+        }
+//        catch (NullPointerException e){      // argumentem w blocku catch jest klasa wyjątku
+//            System.out.println("Brak użytkownika o ID " + userId);
+//            System.out.println();
+//            // e.printStackTrace();         // metoda drukująca na konsoli systemowy komunikat błędu
+//        }
+//        catch(Exception e){
+//            System.out.println("Wystąpił inny błąd");
+//        }
 
     }
 
@@ -77,17 +87,56 @@ public class UserController  implements UserControllerTemplate{
     UserController uc = new UserController();
     uc.registerUser(new User("Test", "Test", "test@test.pl", "tEsT",'K'));
     uc.registerUser(new User("Test2", "Test2", "test2@test.pl", "tEsT2",'M'));
-    System.out.println("Wypisanie wszystkich użytkowników: ");
-      uc.printAllUsers();
-      // Wyrażenie 3-argumentowe: warunek ? wartość jeśli true : wartość jeśli false
-      String login = "test@test.pl";
-      String password = "tEsT2";
-    System.out.println(uc.loginUser(login, password) ? "Zalogowany "+login:"Niezalogowany");
 
-    System.out.println(uc.findUserById(1).getPassword());
-    uc.updatePasswordByUserId(3, "TEEEEEEEEEEEEST1");
-    System.out.println(uc.findUserById(1).getPassword());
+    boolean isRun = true;
+    while (isRun) {
+      // CLI - command line interface
+      System.out.println("APLIKACJA X");
+      System.out.println(
+          "1. Rejestracja \n2. Logowanie \n3. Zmiana hasła \n4. Pokaż wszystkich \n0. Wyjście");
+      Scanner scan = new Scanner(System.in);
 
-    System.out.println(uc.loginUser(login, "TEEEEEEEEEEEEST1"));
+      int choice = scan.nextInt();
+      scan.nextLine();          // instrukcja konsumująca \n po wprowadzonej liczbie, tak aby nie przesuwać kursora
+      switch (choice) {
+        case 1:
+          //uc.registerUser();
+          break;
+        case 2:
+          System.out.println("Podaj email: ");
+          String login = scan.nextLine();
+          System.out.println("Podaj hasło: ");
+          String password = scan.nextLine();
+          System.out.println(uc.loginUser(login, password) ? "Zalogowany "+login:"Niezalogowany");
+          break;
+        case 3:
+          break;
+        case 4:
+          uc.printAllUsers();
+          break;
+        case 0:
+          System.out.print("Wychodzę z programu.");
+          isRun = false;
+          scan.close();
+          break;
+        default:
+          System.out.println("Brak takiej opcji.");
+          break;
+      }
+}
+
+
+//    System.out.println("Wypisanie wszystkich użytkowników: ");
+//      uc.printAllUsers();
+//      // Wyrażenie 3-argumentowe: warunek ? wartość jeśli true : wartość jeśli false
+//      String login = "test@test.pl";
+//      String password = "tEsT2";
+//    System.out.println(uc.loginUser(login, password) ? "Zalogowany "+login:"Niezalogowany");
+//
+//    System.out.println(uc.findUserById(1).getPassword());
+//    uc.updatePasswordByUserId(3, "TEEEEEEEEEEEEST1");
+//    System.out.println(uc.findUserById(1).getPassword());
+//
+//    System.out.println(uc.loginUser(login, "TEEEEEEEEEEEEST1"));
   }
 }

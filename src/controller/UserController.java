789fc72b08382a0f57c93c97c8a8741a50064c16
wiktor.kobiela <wuch.mail@gmail.com>
2020-06-27 12:59:02 +0200

    ref #controller printAllUsers updated

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -2,6 +2,8 @@ package controller;
 
 import model.User;
 
+import java.util.function.Consumer;
+
 // UserControler - klasa kontrolera -> klasa obsługująca i dostarczająca logikę biznesową aplikacji
 public class UserController  implements UserControllerTemplate{
 
@@ -25,6 +27,21 @@ public class UserController  implements UserControllerTemplate{
 
     @Override
     public void printAllUsers() {
+        // 1. W pęti wypisz wszystkie elementy
+//        for(int i=0; i<users.size(); i++){          // WYTYCZNE: stosujemy gdy znana jest liczba iteracji
+//            System.out.println(users.get(i));
+//        }
+//        int i = 0;
+//        while(i < users.size()){                    // WYTYCZNE: stosujemy gdy nieznana jest liczba iteracji
+//            System.out.println(users.get(i));
+//            i++;
+//        }
+//        for(User user : users){                     //for(typElementuKolekcji nazwaTymczasowegoObiektu : kolekcja)
+//            System.out.println(user);               // WYTYCZNE: stosujemy do kolekcji, ale tylko do odczytu
+//        }
+
+        //CTRL _ Spacja -> utwórz predykat-> przekształcenie
+        users.forEach(user -> System.out.println(user));
 
     }
 
@@ -32,5 +49,7 @@ public class UserController  implements UserControllerTemplate{
     UserController uc = new UserController();
     uc.registerUser(new User("Test", "Test", "test@test.pl", "tEsT",'K'));
       uc.registerUser(new User("Test2", "Test2", "test2@test.pl", "tEsT2",'M'));
+    System.out.println("Wypisanie wszystkich użytkowników: ");
+      uc.printAllUsers();
   }
 }

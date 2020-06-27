6df9cba32212d7e42b5bba2140494817de0e4e9e
wiktor.kobiela <wuch.mail@gmail.com>
2020-06-27 09:44:01 +0200

    ref #user first commit

---
+++ src/model/User.java
@@ -0,0 +1,19 @@
+package model;
+//Hints
+// CTRL + D - duplikowanie lini kodu
+
+
+import java.time.LocalDateTime;
+
+//Klasa user - to klasa modelu-> klasa determinująca strukturę danych
+public class User {
+    // pola klasowe -> pola o zasięgu widoczności w obrębie całej klasy
+    String name;
+    String lastName;
+    String email;
+    String password;
+    LocalDateTime registrationDateTime;
+
+
+
+}

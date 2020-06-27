019ea60d95377dd4f66524b2d0b914a22679ae2a
wiktor.kobiela <wuch.mail@gmail.com>
2020-06-27 11:37:45 +0200

    ref #user constructor

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,31 @@ import model.User;
 
 public class Main {
   public static void main(String[] args) {
-        // utworzenie obiektu User
-        User u1 = new User();
-        User u2 = new User();
-        // wywołanie setterów
-        u1.setName("Michał");
-        u2.setName("Anna");
-        u2.setGender('K');
+    // utworzenie obiektu User
+    User u1 = new User("Michał", "Kasztan", "test@test.pl", "kaszana12", 'M');
+    User u2 = new User();
+    // wywołanie setterów
+    u1.setName("Michał");
+    u2.setName("Anna");
+    u2.setGender('K');
 
+    // na obiekcie u1 wywołanie metody toString
+    System.out.println(u1.toString());
+    System.out.println(u2.toString());
+    // wywołanie getterów
+    System.out.println("User 1: " + u1.getName());
+    System.out.println("User 2: " + u2.getName());
+    System.out.println("User 2: " + u2.getGender());
+    // porównanie obiektów
+    System.out.println("Czy obiekty są takie same? " + u1.equals(u2));
 
-        // na obiekcie u1 wywołanie metody toString
-        System.out.println(u1.toString());
-        System.out.println(u2.toString());
-        // wywołanie getterów
-        System.out.println("User 1: " + u1.getName());
-        System.out.println("User 2: " + u2.getName());
-        System.out.println("User 2: " + u2.getGender());
+    User u3 = new User();     // inicjalizuje nowy obiekt
+    u3.setName("Michał");
+
+    User u4 = u1; // obiekt inicjalizowany istniejącą instancją
+    System.out.println("Czy obiekty są takie same? " + u1.equals(u4));
+    u4.setEmail("test@test.pl");
+    System.out.println(u1.toString());
+    System.out.println(u4.toString());
   }
 }
--- src/model/User.java
+++ src/model/User.java
@@ -5,102 +5,135 @@ package model;
 // ALT _ Enter - auto-podpowiedź
 // CTRL + / - komentowanie/odkomentowanie
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 // 1. Wszystkie pola są prywatne
 // 2. Publiczne gettery i settery - metody dostępowe
+// 3. Konstruktor/y
+// 4. Publiczna metoda toString()
 
 // Klasa user - to klasa modelu-> klasa determinująca strukturę danych
-public class User extends Object{       // Każda klasa dziedziczy klasy nadrzędnej (zgodnie ze specyfikatorami dostępu)
-    // pola klasowe -> pola o zasięgu widoczności w obrębie całej klasy
-    private int userId;                                   //domyślnie 0
-    private String name;                                  //domyślnie null
-    private String lastName;                              //domyślnie null
-    private String email;                                 //domyślnie null
-    private String password;                              //domyślnie null
-    private char gender;                                  //domyślnie ''
-    private LocalDateTime registrationDateTime;           //domyślnie null
-    private boolean status;                               //domyślnie false
-
-    // Gettery i Settery
-
-
-    public int getUserId() {
-        return userId;
+public class User
+    extends Object { // Każda klasa dziedziczy klasy nadrzędnej (zgodnie ze specyfikatorami dostępu)
+  // pola klasowe -> pola o zasięgu widoczności w obrębie całej klasy
+  private int userId; // domyślnie 0
+  private String name; // domyślnie null
+  private String lastName; // domyślnie null
+  private String email; // domyślnie null
+  private String password; // domyślnie null
+  private char gender; // domyślnie ''
+  private LocalDateTime registrationDateTime; // domyślnie null
+  private boolean status; // domyślnie false
+  // gdy nie ma konstruktora to zawsze tworzony jest konstruktor domyślny
+    public User(){
+    System.out.println("Jestem w konstruktorze domyślnym");
     }
 
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
+  // konstruktor z wybranymi argumentami
+  // Cel: przypisanie wartości z argumentów do pól klasowych
+  // Gettery i Settery
+  public User(String name, String lastName, String email, String password, char gender) {
+    this.name = name;               // przypisanie do pola klasowego wartości z argumentu
+                                    // this.name -> przypisanie do pola name obiektu który został utworzony wartości z argumentu
+                                    // this -> jest odwołaniem do obiektu czyli jego lokalizacji w pamięci podręcznej
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.gender = gender;
+    this.registrationDateTime = LocalDateTime.now();    // przypisanie aktualnego czasu rejestracji
+      this.status = true;                               // domyślnie status rejestracji jest true
+      this.userId = 1;
+    System.out.println("Jestem w konstruktorze z argumentami");
+  }
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public int getUserId() {
+    return userId;
+  }
 
-    public String getLastName() {
-        return lastName;
-    }
+  public void setUserId(int userId) {
+    this.userId = userId;
+  }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public String getLastName() {
+    return lastName;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public char getGender() {
-        return gender;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public void setGender(char gender) {
-        this.gender = gender;
-    }
+  public String getPassword() {
+    return password;
+  }
 
-    public LocalDateTime getRegistrationDateTime() {
-        return registrationDateTime;
-    }
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
-    public void setRegistrationDateTime(LocalDateTime registrationDateTime) {
-        this.registrationDateTime = registrationDateTime;
-    }
+  public char getGender() {
+    return gender;
+  }
 
-    public boolean isStatus() {
-        return status;
-    }
+  public void setGender(char gender) {
+    this.gender = gender;
+  }
 
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
+  public LocalDateTime getRegistrationDateTime() {
+    return registrationDateTime;
+  }
 
-    // public - widoczna w całym programie
-    // String - typ zwracanej wartości w return
-    // toString() - nazwa metody (nie przyjmuje argumentów)
-    @Override   // Nadpisuje implementacjęmetody toString() z klasy Object
-    public String toString() {
-        return "User{" +
-                "userId=" + userId +
-                ", name='" + name + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", gender=" + gender +
-                ", registrationDateTime=" + registrationDateTime +
-                ", status=" + status +
-                '}';
-    }
+  public void setRegistrationDateTime(LocalDateTime registrationDateTime) {
+    this.registrationDateTime = registrationDateTime;
+  }
+
+  public boolean isStatus() {
+    return status;
+  }
+
+  public void setStatus(boolean status) {
+    this.status = status;
+  }
+
+  // public - widoczna w całym programie
+  // String - typ zwracanej wartości w return
+  // toString() - nazwa metody (nie przyjmuje argumentów)
+  @Override // Nadpisuje implementacjęmetody toString() z klasy Object
+  public String toString() {
+    return "User{"
+        + "userId="
+        + userId
+        + ", name='"
+        + name
+        + '\''
+        + ", lastName='"
+        + lastName
+        + '\''
+        + ", email='"
+        + email
+        + '\''
+        + ", gender="
+        + gender
+        + ", registrationDateTime="
+        + registrationDateTime
+        + ", status="
+        + status
+        + '}';
+  }
 }
